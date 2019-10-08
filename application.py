added simple output, fixed generator loss computation

application.py
@@ -21,8 +21,8 @@ home_dir = str(Path.home()) + '/formelbaer'
 arxiv_dir = home_dir + '/arxiv'
 gen_dir = home_dir + '/generated'
 
-iterations = 1
-discriminator_steps = 1
+iterations = 5
+discriminator_steps = 5
 generator_steps = 1
 sequence_length = 2
 montecarlo_steps = 1
@@ -55,7 +55,7 @@ def main():
 
     # pre training
 
-    for _ in range(iterations):
+    for iteration in range(iterations):
         for _ in range(discriminator_steps):
             
             samples = generator.rollout(batch_size, sequence_length)
@@ -63,19 +63,20 @@ def main():
             discriminator.train(gen_dir, batch_size)
             clear(gen_dir)
 
-        generator.updaterollout()
+        generator.update_rollout()
 
         for _ in range(generator_steps):
             
             for length in range(sequence_length):
                 batch, h = generator.step(batch_size)
-                rewards = torch.empty([batch.shape[0],0])
+                rewards = torch.empty([batch_size,0])
 
                 for _ in range(montecarlo_steps):
                     missing = sequence_length - length
                     samples = generator.rollout(batch_size, missing, batch, h)
                     convert(samples, gen_dir)
                     reward = discriminator.rewards(gen_dir, batch_size)
+                    reward = reward[:,None]
                     rewards = torch.cat([rewards, reward], dim=1)
 
                     clear(gen_dir)
@@ -83,7 +84,21 @@ def main():
                     rewards = torch.mean(rewards, dim=1)
                     generator.feedback(rewards)
 
-            generator.updatepolicy()
+            generator.update_policy()
+
+        if iterations % 1 == 0:
+
+            gloss = generator.running_loss / (iteration+1 * generator_steps)
+            dloss = discriminator.running_loss / (iteration+1 * discriminator_steps)
+
+            print('###')
+            print('Iteration {}'.format(iteration))
+            print('Generator Loss {}'.format(gloss))
+            print('Discriminator Loss {}'.format(dloss))
+            print('###')
+
+            generator.running_loss = 0.0
+            discriminator.running_loss = 0.0
 
 
 if __name__ == "__main__":

generator.py
@@ -14,6 +14,8 @@ eps = finfo(float32).eps.item()
 rollout_net = PolicyNetwork().to(device).eval()
 rollout_net.load_state_dict(policy_net.state_dict())
 
+running_loss = 0.0
+
 
 def step(batch_size, batch=None, h=None):
 
@@ -63,10 +65,11 @@ def feedback(reward):
     policy_net.rewards.append(reward)
 
 
-def updatepolicy():
+def update_policy():
+    global running_loss
 
     total = 0
-    policy_loss = []
+    loss = []
     returns = []
 
     for r in policy_net.rewards[::-1]:
@@ -77,19 +80,19 @@ def updatepolicy():
     returns = (returns - returns.mean()) / (returns.std() + eps)
 
     for log_prob, total in zip(policy_net.probs, returns):
-        policy_loss.append(-log_prob * total)
+        loss.append(-log_prob * total)
 
-    optimizer.zero_grad()
-
-    policy_loss = torch.cat(policy_loss).sum()
-    policy_loss.backward()
-
+    loss = torch.cat(loss).sum()
+    running_loss += loss.item()
+    
+    loss.backward()
     optimizer.step()
+    optimizer.zero_grad()
 
     del policy_net.rewards[:]
     del policy_net.probs[:]
 
 
-def updaterollout():
+def update_rollout():
 
     rollout_net.load_state_dict(policy_net.state_dict())

nn_discriminator.py
@@ -24,9 +24,9 @@ class Discriminator(nn.Module):
 
 		self.fc3 = nn.Linear(5 * 32, 64)
 		self.fc4 = nn.Linear(64, 32)
-		self.fc5 = nn.Linear(32,1)
+		self.fc5 = nn.Linear(32,2)
 
-		self.relu = nn.ReLU()
+		self.softmax = nn.Softmax(dim=0)
 
 	def forward(self,x):
 
@@ -44,13 +44,13 @@ class Discriminator(nn.Module):
 
 		out = out.view(-1, 5 * 32)
 		out = self.fc3(out)
-		out = self.relu(out)
+		out = gelu(out)
 
 		out = self.fc4(out)
-		out = self.relu(out)
+		out = gelu(out)
 
 		out = self.fc5(out)
-		out = self.relu(out)
+		out = self.softmax(out)
 
 		return out
 

nn_policy.py
@@ -26,7 +26,7 @@ class PolicyNetwork(nn.Module):
 
         self.lin = nn.Linear(hidden_dim, output_dim)
         self.relu = nn.ReLU()
-        self.softmax = nn.Softmax(dim=1)
+        self.softmax = nn.Softmax(dim=0)
 
         self.probs = []
         self.rewards = []