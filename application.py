fixed precompilation of tex preamble, fixed generator gradient increment computation, added docker files, added constants again, fixed default batch in generator.py

application.py
@@ -8,39 +8,27 @@ import datetime
 import shutil
 import os
 
+import constants as c
+
 from pathlib import Path
 from converter import convert
+
+
+app_dir = c.DIRECTORY_APPLICATION
+arxiv_dir = c.DIRECTORY_ARXIV_DATA
+gen_dir = c.DIRECTORY_GENERATED_DATA
+
+iterations = c.ADVERSARIAL_ITERATIONS
+discriminator_steps = c.ADVERSARIAL_DISCRIMINATOR_STEPS
+generator_steps = c.ADVERSARIAL_GENERATOR_STEPS
+sequence_length = c.ADVERSARIAL_SEQUENCE_LENGTH
+montecarlo_steps = c.ADVERSARIAL_MONTECARLO
+batch_size = c.ADVERSARIAL_PREFERRED_BATCH_SIZE
 
 
 # TODO malus fuer syntaktisch inkorrekte baeume
 # TODO training output
 # TODO irgendwie uebersichtlicher machen?
-
-
-home_dir = str(Path.home()) + '/formelbaer'
-arxiv_dir = home_dir + '/arxiv'
-gen_dir = home_dir + '/generated'
-
-iterations = 4
-discriminator_steps = 2 # (*2) due to implementation
-generator_steps = 1
-sequence_length = 6
-montecarlo_steps = 6
-
-batch_size = 32
-
-
-def init():
-
-    if not os.path.exists(home_dir):
-        os.makedirs(home_dir)
-
-    if not os.path.exists(gen_dir):
-        os.makedirs(gen_dir)
-
-    if not os.path.exists(arxiv_dir):
-        raise ValueError('Please save the training set of arxiv .png or .pt files in ' \
-            + arxiv_dir + ' or change the ARXIV directory in application.py accordingly.')
 
 
 def clear(folder):
@@ -51,16 +39,14 @@ def clear(folder):
 
 def main():
 
-    init()
-
     # pre training
 
     for iteration in range(iterations):
         for _ in range(discriminator_steps):
             
-            samples = generator.rollout(batch_size, sequence_length)
+            samples = generator.rollout(sequence_length, batch_size=batch_size)
             convert(samples, gen_dir)
-            discriminator.train(gen_dir, batch_size)
+            discriminator.train()
             clear(gen_dir)
 
         generator.update_rollout()
@@ -68,14 +54,14 @@ def main():
         for _ in range(generator_steps):
             
             for length in range(sequence_length):
-                batch, h = generator.step(batch_size)
+                batch, h = generator.step(batch_size=batch_size)
                 rewards = torch.empty([batch_size,0])
 
                 for _ in range(montecarlo_steps):
                     missing = sequence_length - length
-                    samples = generator.rollout(batch_size, missing, batch, h)
+                    samples = generator.rollout(missing, batch, h)
                     convert(samples, gen_dir)
-                    reward = discriminator.rewards(gen_dir, batch_size)
+                    reward = discriminator.rewards(gen_dir)
                     reward = reward[:,None]
                     rewards = torch.cat([rewards, reward], dim=1)
 

discriminator.py
@@ -1,31 +1,34 @@
 import application
 import torch
 import random
-from nn_discriminator import Discriminator, learnrate
+import constants as c
+from nn_discriminator import Discriminator
 from dataset import Dataset
 from PIL import Image
 from torch.utils.data import DataLoader
 from torchvision import transforms, datasets
 
 
+
 device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+
 model = Discriminator().to(device)
-optimizer = torch.optim.Adam(model.parameters(), lr=learnrate)
+optimizer = torch.optim.Adam(model.parameters(), lr=c.DISCRIMINATOR_LEARNRATE)
 criterion = torch.nn.BCELoss()
-transform = transforms.Compose(
-    [lambda img: img.convert(mode='LA'),
-    transforms.CenterCrop((32, 333)),
-    transforms.ToTensor(),
-    lambda img: img[1]])
-arxiv_data = Dataset(application.arxiv_dir, label=0, transform=transform)
+
+transform = transforms.Compose([lambda img: img.convert(mode='LA'),
+    transforms.CenterCrop((32, 333)),transforms.ToTensor(),lambda img: img[1]])
+arxiv_data = Dataset(c.DIRECTORY_ARXIV_DATA, label=0, transform=transform)
+
 running_loss = 0.0
 
 
-def rewards(folder, batch_size):
+def rewards(folder):
 
     model.eval()
 
     dataset = Dataset(folder=folder, label=1, transform=transform)
+    batch_size = len(dataset)
     loader = DataLoader(dataset, batch_size)
 
     images, _ = next(iter(loader))
@@ -35,13 +38,15 @@ def rewards(folder, batch_size):
 
     return rewards[:,1] # [:,1] probability that p ~ real distribution
 
-def train(folder, batch_size):
+def train():
     global running_loss
 
     model.train()
     optimizer.zero_grad()
 
-    generated_data = Dataset(folder, label=1, transform=transform)
+    generated_data = Dataset(c.DIRECTORY_GENERATED_DATA, label=1, transform=transform)
+    batch_size = min(len(generated_data),len(arxiv_data))
+
     generated_data.append(arxiv_data.random(amount=batch_size))
 
     loader = DataLoader(generated_data, batch_size)

generator.py
@@ -1,63 +1,85 @@
 from numpy.random import choice
 from numpy import empty, finfo, float32
-from nn_policy import PolicyNetwork, learnrate, baseline, gamma
+from nn_policy import PolicyNetwork
 
 import tokens
 import torch
 
-
-device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
-policy_net = PolicyNetwork().to(device)
-optimizer = torch.optim.Adam(policy_net.parameters(), lr=learnrate)
-eps = finfo(float32).eps.item()
-
-rollout_net = PolicyNetwork().to(device).eval()
-rollout_net.load_state_dict(policy_net.state_dict())
+hidden_dim = 32
+layers = 2
+dropout = 0.2
+learnrate = 0.01
+baseline = 1
+gamma = 0.95
 
 running_loss = 0.0
 
+device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 
-def step(batch_size, batch=None, h=None):
+policy_net = PolicyNetwork(hidden_dim, layers, dropout).to(device)
+rollout_net = PolicyNetwork(hidden_dim, layers, dropout).to(device).eval()
+rollout_net.load_state_dict(policy_net.state_dict())
 
-    batch = torch.zeros(batch_size,1,tokens.count()) if batch is None else batch
-    h = policy_net.init_hidden(batch.shape[0]) if h is None else h
+optimizer = torch.optim.Adam(policy_net.parameters(), lr=learnrate)
+eps = finfo(float32).eps.item()
+
+
+def step(batch=None, hidden=None, batch_size=None):
+
+    if batch_size is None and batch is None:
+        raise ValueError('Either specify a batch size or provide a batch.')
+
+    # generate a new batch of sequences
+    if batch == None:
+        optimizer.zero_grad()
+
+        batch = torch.zeros(batch_size,1,tokens.count())
+        hidden = policy_net.init_hidden(layers, batch_size, hidden_dim)
+
+        batch.requires_grad = False
+        batch.to(device)
 
     policy_net.train()
 
-    batch.requires_grad = False
-    batch.to(device)
-
-    policies, actions, batch, h = decision(policy_net, batch, h)
+    policies, actions, batch, hidden = decision(policy_net, batch, hidden)
     policy_net.probs.append(policies.log_prob(actions))
 
-    return batch, h
+    return batch, hidden
 
 
-def rollout(batch_size, sequence_length, batch=None, h=None):
+def rollout(length, batch=None, hidden=None, batch_size=None):
+
+    if batch_size is None and batch is None:
+        raise ValueError('Either specify a batch size or provide a batch.')
 
     with torch.no_grad():
 
-        batch = torch.zeros([batch_size,1,tokens.count()]) if batch is None else batch
-        h = rollout_net.init_hidden(batch.shape[0]) if h is None else h
+        if batch is None:
+            batch = torch.zeros([batch_size,1,tokens.count()])
+            hidden = rollout_net.init_hidden(layers,batch_size,hidden_dim)
 
-        for _ in range(sequence_length - batch.shape[1]):
-            _, _, batch, h = decision(rollout_net, batch, h)
+        for _ in range(length - batch.shape[1]):
+            _, _, batch, hidden = decision(rollout_net, batch, hidden)
 
     return batch
 
 
-def decision(net, batch, h):
+def decision(net, batch, hidden):
 
+    # use hidden state to avoid feeding whole sequences redundantly
     state = batch[:,-1,:][:,None,:]
-    policies, h = net(state, h)
+    policies, hidden = net(state, hidden)
+
+    # sample next actions
     policies = torch.distributions.Categorical(policies)
     actions = policies.sample() 
 
+    # concat onehot tokens with the batch of sequences
     encodings = torch.Tensor(list(map(tokens.onehot, actions)))
     encodings = encodings[:,None,:]
     batch = torch.cat((batch,encodings),dim=1)
 
-    return policies, actions, batch, h
+    return policies, actions, batch, hidden
 
 
 def feedback(reward):
@@ -69,25 +91,33 @@ def update_policy():
     global running_loss
 
     total = 0
-    loss = []
+    increment = []
     returns = []
 
+    # compute (state,action) values for each step
     for r in policy_net.rewards[::-1]:
         total = r + gamma * total
         returns.insert(0, total)
 
-    returns = returns[0]
-    returns = (returns - returns.mean()) / (returns.std() + eps)
+    # for each step standardize rewards in batch
+    for i in range(len(returns)):
+        returns[i] = (returns[i] - returns[i].mean()) / (returns[i].std() + eps)
 
-    for log_prob, total in zip(policy_net.probs, returns):
-        loss.append(-log_prob * total)
+    # calculate rewards weighted by log probability
+    for log_prob, reward in zip(policy_net.probs, returns):
+        increment.append(-log_prob * reward)
 
-    loss = torch.cat(loss).sum()
-    running_loss += loss.item()
-    
-    loss.backward()
+    # sum rewards over all steps for each sample
+    increment = torch.stack(increment)
+    increment = torch.sum(increment, dim=1)
+
+    # average rewards over batch
+    batch_size = increment.shape[0]
+    increment = torch.sum(increment) / batch_size
+
+    running_loss += increment.item()
+    increment.backward()
     optimizer.step()
-    optimizer.zero_grad()
 
     del policy_net.rewards[:]
     del policy_net.probs[:]

nn_policy.py
@@ -4,22 +4,16 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+
 input_dim = tokens.count()
 output_dim = tokens.count()
-
-hidden_dim = 32
-layers = 2
-dropout = 0.2
-learnrate = 0.01
-baseline = 1
-gamma = 0.95
 
 device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 
 
 class PolicyNetwork(nn.Module):
 
-    def __init__(self):
+    def __init__(self, hidden_dim, layers, dropout):
         super(PolicyNetwork, self).__init__()
         
         self.gru = nn.GRU(input_dim,hidden_dim,layers,batch_first=True,dropout=dropout)
@@ -42,6 +36,6 @@ class PolicyNetwork(nn.Module):
 
         return out, h
     
-    def init_hidden(self, batch_size):
+    def init_hidden(self, layers, batch_size, hidden_dim):
 
         return torch.zeros(layers, batch_size, hidden_dim).to(device)