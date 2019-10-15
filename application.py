fixed all numerical loss problems

application.py
@@ -13,14 +13,13 @@ import os
 
 
 # TODO wieso kann ich negative rewards bekommen??
+# TODO wieso benutzen seqgans cross entropy und keine verteilungs divergenz?
 
 
 def generator_training(nn_policy, nn_rollout, nn_discriminator, nn_oracle, g_opt, o_crit):
 
     nn_policy.train()
     nn_rollout.eval()
-
-    nn_rollout.set_parameters_to(nn_policy)
 
     for _ in range(cfg.app_cfg.g_steps):
         batch, hidden = nn_policy.initial()
@@ -72,13 +71,14 @@ def adversarial_training():
     d_opt = torch.optim.Adam(nn_discriminator.parameters(), lr=cfg.d_cfg.learnrate)
     d_crit = torch.nn.BCELoss()
     g_opt = torch.optim.Adam(nn_policy.parameters(), lr=cfg.g_cfg.learnrate)
-    o_crit = torch.nn.CrossEntropyLoss()
+    o_crit = torch.nn.KLDivLoss()
 
     # START ADVERSARIAL TRAINING
 
     log.start_experiment()
 
     for i in range(cfg.app_cfg.iterations):
+        nn_rollout.set_parameters_to(nn_policy)
 
         discriminator_training(nn_discriminator, nn_rollout, d_opt, d_crit)
         generator_training(nn_policy, nn_rollout, nn_discriminator, nn_oracle, g_opt, o_crit)
@@ -88,9 +88,9 @@ def adversarial_training():
     # FINISH EXPERIMENT AND WRITE LOGS
 
     directory = loader.get_experiment_directory()
-    nn_policy.save(directory + '/policy.pt')
-    nn_discriminator.save(directory + '/discriminator.pt')
-    nn_oracle.save(directory + '/oracle.pt')
+    nn_policy.save(directory + '/policy_net.pt')
+    nn_discriminator.save(directory + '/discriminator_net.pt')
+    nn_oracle.save(directory + '/oracle_net.pt')
 
     log.finish_experiment(directory)
 

config.py
@@ -62,12 +62,12 @@ class DiscriminatorConfig:
 DEFAULT_PATHS_CFG = Paths(
 
 	app = str(pathlib.Path.home()) + '/formelbaer_data',
-	synthetic_data = str(pathlib.Path.home()) + '/formelbaer_data/synthetic',
-	arxiv_data = str(pathlib.Path.home()) + '/formelbaer_data/arxiv',
-	oracle_data = str(pathlib.Path.home()) + '/formelbaer_data/oracle',
+	synthetic_data = str(pathlib.Path.home()) + '/formelbaer_data/synthetic_samples',
+	arxiv_data = str(pathlib.Path.home()) + '/formelbaer_data/arxiv_samples',
+	oracle_data = str(pathlib.Path.home()) + '/formelbaer_data/oracle_samples',
 	
 	log = str(pathlib.Path.home()) + '/formelbaer_data/results.log',
-	oracle = str(pathlib.Path.home()) + '/formelbaer_data/oracle.pt'
+	oracle = str(pathlib.Path.home()) + '/formelbaer_data/oracle_net.pt'
 
 	)
 
@@ -91,14 +91,14 @@ DEFAULT_APP_CFG = AppConfig(
 
 	device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu'),
 
-	iterations = 2,
+	iterations = 4,
 	d_steps = 2, # (*2) due to computational cost reasons
 	g_steps = 1,
 	seq_length = 2, # 16
 	montecarlo_trials = 2, # 16
 	batchsize = multiprocessing.cpu_count()*4, # computational cost reasons
 
-	oracle = False,
+	oracle = True,
 	oracle_samplesize = 100,
 
 	label_synth = 1,

generator.py
@@ -2,7 +2,9 @@ from numpy import finfo, float32
 
 import tokens
 import torch
+import math
 import config as cfg
+
 
 def step(nn_policy, batch, hidden, nn_oracle, o_crit):
 
@@ -12,17 +14,25 @@ def step(nn_policy, batch, hidden, nn_oracle, o_crit):
 
     # in case of oracle training compute oracle loss
     if cfg.app_cfg.oracle and nn_oracle is not None:
-        _, hidden = nn_oracle.inital()
+        _, hidden = nn_oracle.initial()
         oracle_policies, _ = nn_oracle(batch, hidden)
-        loss = o_crit(policies, oracle_policies)
+
+        # kl divergence module expects input prob logs
+        log_policies = torch.log(policies)
+
+        loss = o_crit(log_policies, oracle_policies)
         nn_oracle.running_loss += loss.item()
 
     # sample next actions
     policies = torch.distributions.Categorical(policies)
     actions = policies.sample() 
 
-    # save probabilities for gradient computation
-    nn_policy.probs.append(policies.log_prob(actions))
+    # actions is a tensor of size batchsize with an indices in range num_features
+    # each index can be seen as a choice for the action with the respective int code
+
+    # save log probabilities for gradient computation
+    log_probs = policies.log_prob(actions) 
+    nn_policy.probs.append(log_probs)
 
     # concat onehot tokens with the batch of sequences
     encodings = torch.Tensor(list(map(tokens.onehot, actions)))
@@ -66,7 +76,7 @@ def reward(nn_policy, rewards):
 def update(nn_policy, optimizer):
 
     total = 0
-    increment = []
+    loss = []
     returns = []
 
     # compute state action values for each step
@@ -81,19 +91,26 @@ def update(nn_policy, optimizer):
 
     # weight state action values by log probability of action
     for log_prob, reward in zip(nn_policy.probs, returns):
-        increment.append(-log_prob * reward)
+        loss.append(-log_prob * reward)
 
     # sum rewards over all steps for each sample
-    increment = torch.stack(increment)
-    increment = torch.sum(increment, dim=1)
+    loss = torch.stack(loss)
+    loss = torch.sum(loss, dim=1)
 
     # average rewards over batch
-    batchsize = increment.shape[0]
-    increment = torch.sum(increment) / batchsize
+    batchsize = loss.shape[0]
+    loss = torch.sum(loss) / batchsize
 
-    nn_policy.running_reward += -1 * increment.item()
-    increment.backward()
+    loss.backward()
     optimizer.step()
+
+    # max reward * prob ~> max reward * log prob ~> min -(reward * log prob)
+    # the loss is positive (even if we multiplied with - log_prob) because the log prob
+    # itsself is negative. instead of starting negative and max reward we start positive
+    # and minimize the negative reward. for clarity we still keep track of the negative
+    # reward we're going to max
+
+    nn_policy.running_reward += -1 * loss.item()
 
     del nn_policy.rewards[:]
     del nn_policy.probs[:]

nn_policy.py
@@ -20,7 +20,7 @@ class Policy(nn.Module):
         self.gru = nn.GRU(self.input_dim,self.hidden_dim,self.layers,batch_first=True,dropout=self.dropout)
         self.lin = nn.Linear(self.hidden_dim, self.output_dim)
         self.relu = nn.ReLU()
-        self.softmax = nn.Softmax(dim=0)
+        self.softmax = nn.Softmax(dim=1) # in forward (batch_size, num_features)
 
         if oracle:
             for param in self.parameters():
@@ -69,7 +69,7 @@ class Policy(nn.Module):
 class Oracle(Policy):
 
     def __init__(self):
-        super().__init__(oracle=True)
+        super(Oracle, self).__init__(oracle=True)
 
         self.eval()
         self.running_loss = 0.0