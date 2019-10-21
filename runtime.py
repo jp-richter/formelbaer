41e1026cc87ead6df85478fa2e922b7ea96a26f4
jp-richter <jan-philip.richter@tu-dortmund.de>
2019-10-21 20:18:05 +0200

    fixed matplotlib import

--- runtime.py
+++ runtime.py
@@ -1,5 +1,5 @@
 # This import registers the 3D projection, but is otherwise unused.
-from mpl_toolkits.mplot3d import Axes3D  # noqa: F401 unused import
+# from mpl_toolkits.mplot3d import Axes3D  # noqa: F401 unused import
 from matplotlib.ticker import LinearLocator, FormatStrFormatter
 
 import matplotlib.pyplot as plt
