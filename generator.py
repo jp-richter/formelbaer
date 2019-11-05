cb444c9c4b5cf8d7c72e2da7585edf20635f4848
jp-richter <jan-philip.richter@tu-dortmund.de>
2019-11-05 21:17:15 +0100

    params

--- generator.py
+++ generator.py
@@ -185,7 +185,7 @@ def policy_gradient_update(nn_policy):
 
     # substract baseline
     for length in range(len(returns)):
-        returns[length] = returns[length] - (0.055 * (config.general.sequence_length - length))
+        returns[length] = returns[length] - (0.05 * (config.general.sequence_length - length))
 
     # weight state action values by log probability of action
     for log_prob, reward in zip(nn_policy.probs, returns):
